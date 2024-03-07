df7b01a41fc4f81f3ae7b0d099530f373238415f
Yash <ustavpatel2004@gmail.com>
2024-03-07 17:56:45 +0530

    DSA

--- basic.c
+++ basic.c
@@ -2,9 +2,9 @@
 
 int main()
 {
-    int a=1,b=2,c=1,d;
-    d=--a && ++b|| ++c;
-    printf("a: %d,b: %d,c: %d,d: %d",a,b,c,d);
+    int a=1,b=2,c;
+    c=++a && ++b;
+    printf("a: %d,b: %d,c: %d",a,b,c);
 
 
 
---
+++ p1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main()
+{
+    int a=11;
+
+   printf("a: %d,a: %d",a,a++);
+   printf("a: %d,a: %d",a++,a++); 
+   printf("a: %d,a: %d",++a,a++); 
+   printf("a: %d,a: %d",a++,++a); 
+   printf("a: %d,a: %d",a,++a);
+   printf("a: %d,a: %d",++a,++a);
+    
+
+}
