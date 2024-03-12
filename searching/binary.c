d329d762771e1d1fbeeb4465e3d9f70ae9c5d219
Yash <ustavpatel2004@gmail.com>
2024-03-12 17:08:18 +0530

    DSA

--- searching/binary.c
+++ searching/binary.c
@@ -1,59 +1,48 @@
-#include<stdio.h>
+#include <stdio.h>
 int main()
 {
-    int i,n;
+    int i, n;
 
     printf("Enter the number of element:- ");
-    scanf("%d",&n);
+    scanf("%d", &n);
 
-    int arr[n],l,h,mid,check=0,num;
+    int arr[n], l, h, mid, check = 0, num;
 
-    for(i=0;i<n;i++)
+    for (i = 0; i < n; i++)
     {
         printf("Enter the number:- ");
-        scanf("%d",&arr[i]);
-
+        scanf("%d", &arr[i]);
     }
     printf("Enter the number of search:- \n");
-    scanf("%d",&num);
-    l=0;
-    h=n-1;
-    mid=(l+h)/2;
+    scanf("%d", &num);
+    l = 0;
+    h = n-15;
+    mid = (l + h) / 2;
 
-   
-
-    while(check==0)
+    while (l <= h)
     {
-      
-        if(arr[mid]==num)
+        mid = (l + h) / 2;
+        if (arr[mid] == num)
         {
-            check=1;
+            check = 1;
             break;
         }
-        else  if(arr[mid]<n)
+        else if (arr[mid] < n)
         {
-            h=mid+1;
-            mid=(l+h)/2;
+            l = mid + 1;
         }
-        else if(arr[mid]>n)
+        else if (arr[mid] > n)
         {
-            l=mid-1;
-            mid=(l+h)/2;
+            h = mid - 1;
         }
-      
     }
-   if(check==1)
-   {
-    printf("this element position:- %d",mid);
-   }
-   else
-   {
+
+    if (check == 1)
+    {
+        printf("this element position:- %d", mid + 1);
+    }
+    else
+    {
         printf("this is not element in array");
-   }
-    
-
-
-
+    }
 }
-
-    
